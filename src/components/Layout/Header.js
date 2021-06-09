3cbb44e50302204a1dc6c9ef517f382e193f37db
Derek Murphy <derekmurphy1993@gmail.com>
2021-06-09 17:42:56 -0400

    Started to Impliment Cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
     return <React.Fragment>
         <header className={classes.header}>
             <h1>ReactFood</h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShowCart}/>
         </header>
         <div className={classes['main-image']}><img src={mealsImage} alt="table of food" /></div>
     </React.Fragment>
