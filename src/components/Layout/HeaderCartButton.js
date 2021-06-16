a5ca8a43554b6e14f5742316fbf27fdff0718e62
Derek Murphy <derekmurphy1993@gmail.com>
2021-06-16 16:47:19 -0400

    Added reducer logic and button animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
 import CartIcon from "./CartIcon"
 import classes from "./HeaderCartButton.module.css"
 import CartContext from '../../store/cart-context'
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 
 const HeaderCartButton = props => {
+    const [btnHighlight, setBtnHighlight] = useState(false)
     const cartCtx = useContext(CartContext)
 
-    const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
+    const { items } = cartCtx
+
+    const numberOfCartItems = items.reduce((currentNum, item) => {
         return currentNum + item.amount
     }, 0)
 
-    return <button className={classes.button} onClick={props.onClick}>
+    const btnClasses = `${classes.button} ${ btnHighlight ? classes.bump : ''}`
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return
+        }
+        setBtnHighlight(true)
+    const timer = setTimeout(() => {
+            setBtnHighlight(false)
+        }, 300)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items])
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
